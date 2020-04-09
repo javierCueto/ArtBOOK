aed2519c8f6d8a086bc24c311ace90a86f482c44
Javier <javelie@outlook.com>
2020-04-08 22:23:16 -0600

    color navigation controller and second view

---
+++ ArtBOOK/DetailsVCViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailsVCViewController.swift
+//  ArtBOOK
+//
+//  Created by José Javier Cueto Mejía on 08/04/20.
+//  Copyright © 2020 Pozolx. All rights reserved.
+//
+
+import UIKit
+
+class DetailsVCViewController: UIViewController {
+    
+    @IBOutlet weak var artImage: UIImageView!
+    
+    @IBOutlet weak var nameLable: UITextField!
+    
+    @IBOutlet weak var artistLabel: UITextField!
+    
+    
+    @IBOutlet weak var yearLabel: UITextField!
+    
+    
+    
+    @IBAction func saveButton(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
