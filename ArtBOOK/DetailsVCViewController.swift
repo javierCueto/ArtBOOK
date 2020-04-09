946e692e230c8243da1eb17d48652248d1bc23d9
Javier <javelie@outlook.com>
2020-04-09 12:17:52 -0600

    select photo from library

--- ArtBOOK/DetailsVCViewController.swift
+++ ArtBOOK/DetailsVCViewController.swift
@@ -20,15 +20,46 @@ class DetailsVCViewController: UIViewController {
 
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
+        
+        artImage.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        artImage.addGestureRecognizer(imageTapRecognizer)
     }
        
     @objc func hideKeyboard(){
         view.endEditing(true)
     }
+    
+    
+    @objc func selectImage(){
+        print("image tap")
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        
+        picker.sourceType = .photoLibrary
+        
+        picker.allowsEditing = true
+        
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    
     @IBAction func saveButton(_ sender: Any) {
         
     }
    
 
 
+}
+
+extension DetailsVCViewController: UIImagePickerControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        artImage.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension DetailsVCViewController: UINavigationControllerDelegate{
+    
 }
