07bef78aadfdfe022f9883145ac36c1212f770f4
Javier <javelie@outlook.com>
2020-04-10 19:08:54 -0600

    save data and show data (Fix interface)

--- ArtBOOK/DetailsVCViewController.swift
+++ ArtBOOK/DetailsVCViewController.swift
@@ -9,41 +9,121 @@
 import UIKit
 import CoreData
 
-class DetailsVCViewController: UIViewController {
+class DetailsVCViewController: UIViewController,UIScrollViewDelegate {
+    var keySize : CGFloat?
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imageView: UIView!
     @IBOutlet weak var artImage: UIImageView!
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var artistText: UITextField!
     @IBOutlet weak var yearText: UITextField!
+    @IBOutlet weak var saveDataButton: UIButton!
     var chosenPainting = ""
     var chosenPaintingId : UUID?
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //scrollView.delegate = self
+        // call the 'keyboardWillShow' function when the view controller receive the notification that a keyboard is going to be shown
+          NotificationCenter.default.addObserver(self, selector: #selector(DetailsVCViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+            // call the 'keyboardWillHide' function when the view controlelr receive notification that keyboard is going to be hidden
+          NotificationCenter.default.addObserver(self, selector: #selector(DetailsVCViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
         if chosenPainting != ""{
+            saveDataButton.isHidden = true
             //Query here
-            let stringUUID = chosenPaintingId!.uuidString
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
             
-            print(stringUUID)
+            let idString = chosenPaintingId?.uuidString
+            
+            
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
+            do{
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                        if let name = result.value(forKey: "name") as? String {
+                            nameText.text = name
+                            nameText.isEnabled = false
+                        }
+                        
+                        if let year = result.value(forKey: "year") as? Int {
+                            yearText.text = String(year)
+                            yearText.isEnabled = false
+                        }
+                        
+                        if let artist = result.value(forKey: "artist") as? String {
+                            artistText.text = artist
+                            artistText.isEnabled = false
+                        }
+                        
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            artImage.image = image
+                        }
+                        
+                    }
+                }
+            }catch{
+                
+            }
+            
+            
         } else {
-            nameText.text = ""
-            artistText.text = ""
-            yearText.text = ""
+            saveDataButton.isEnabled = false
+            
+            let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+            view.addGestureRecognizer(gestureRecognizer)
+            
+            artImage.isUserInteractionEnabled = true
+            let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+            artImage.addGestureRecognizer(imageTapRecognizer)
         }
         
         
         
         
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-        view.addGestureRecognizer(gestureRecognizer)
         
-        artImage.isUserInteractionEnabled = true
-        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
-        artImage.addGestureRecognizer(imageTapRecognizer)
+    }
+    
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(keySize)
+      self.view.frame.origin.y = 0 - (keySize ?? 0)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+           // if keyboard size is not available for some reason, dont do anything
+            print("error al ocultar")
+           return
+        }
+      
+
+      // move the root view up by the distance of keyboard height
+        print("se movio arriba")
+
+      
+      self.view.frame.origin.y = 0 - keyboardSize.height
+        keySize = keyboardSize.height
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+      // move back the root view origin to zero
+        print("se oculto")
     }
     
     @objc func hideKeyboard(){
+        self.view.frame.origin.y = 0
         view.endEditing(true)
+        keySize = 0
     }
     
     
@@ -62,14 +142,14 @@ class DetailsVCViewController: UIViewController {
     
     
     @IBAction func saveButton(_ sender: Any) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
         let context = appDelegate.persistentContainer.viewContext
         
-       
+        
         let newPainting = NSEntityDescription.insertNewObject(forEntityName : "Paintings", into: context)
         
         if let year = Int(yearText.text!) {
-             newPainting.setValue(year, forKey: "year")
+            newPainting.setValue(year, forKey: "year")
         }
         newPainting.setValue(nameText.text, forKey: "name")
         newPainting.setValue(artistText.text, forKey: "artist")
@@ -104,6 +184,7 @@ class DetailsVCViewController: UIViewController {
 extension DetailsVCViewController: UIImagePickerControllerDelegate{
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         artImage.image = info[.originalImage] as? UIImage
+        saveDataButton.isEnabled = true
         self.dismiss(animated: true, completion: nil)
     }
 }
--- ArtBOOK/ViewController.swift
+++ ArtBOOK/ViewController.swift
@@ -48,17 +48,20 @@ class ViewController: UIViewController {
         
         do{
            let results = try context.fetch(fetchRequest)
-            for result in results as! [NSManagedObject]{
-                if let name = result.value(forKey: "name") as? String {
-                    self.nameArray.append(name)
-                }
+            if results.count > 0 {
+                for result in results as! [NSManagedObject]{
+                    if let name = result.value(forKey: "name") as? String {
+                        self.nameArray.append(name)
+                    }
+                    
+                    if let id = result.value(forKey: "id") as? UUID{
+                        self.idArray.append(id)
+                    }
                 
-                if let id = result.value(forKey: "id") as? UUID{
-                    self.idArray.append(id)
                 }
-            
+                self.tableView.reloadData()
             }
-            self.tableView.reloadData()
+            
         }catch{
             print("error when load all data")
         }
@@ -107,6 +110,45 @@ extension ViewController: UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            
+            let idString = idArray[indexPath.row].uuidString
+            
+            
+            
+            fetchRequest.predicate = NSPredicate(format: " id = %@", idString)
+            
+            fetchRequest.returnsObjectsAsFaults = false
+            do{
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let id = result.value(forKey: "id") as? UUID{
+                            if id == idArray[indexPath.row] {
+                                context.delete(result)
+                                nameArray.remove(at: indexPath.row)
+                                idArray.remove(at: indexPath.row)
+                                self.tableView.reloadData()
+                                
+                                try context.save()
+                                
+                                break
+                            }
+                        }
+                    }
+                }
+            }catch{
+                
+            }
+            
+        }
+    }
     
 }
 
